df5b7f9db52023e906b5f56a4c590601745be88e
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-07-04 11:36:21 -0300

    projeto iniciado

---
+++ src/test/java/com/pedroacbg/ntconsult/NtconsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.ntconsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtconsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
