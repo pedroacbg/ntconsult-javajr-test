6528b8ffa6c46a5636036a27a7dec1949335bb15
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-07-04 11:52:00 -0300

    entidade Livro criada

---
+++ src/main/java/com/pedroacbg/ntconsult/model/Livro.java
@@ -0,0 +1,35 @@
+package com.pedroacbg.ntconsult.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_livro")
+@Data
+@NoArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 150, nullable = false)
+    private String titulo;
+
+    @Column(length = 100, nullable = false)
+    private String autor;
+
+    @Column(length = 4, nullable = false)
+    private Integer anoPublicacao;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+}
